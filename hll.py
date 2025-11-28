45c2824cddeafff450909cc8eade92e254e6ef88
Sebastian <sebastian.balsam@posteo.de>
2025-11-28 23:30:17 +0100

    finished HLL implementation

--- hll.py
+++ hll.py
@@ -1,40 +1,45 @@
+import math
 import numpy as np
 
 # -*- coding: utf-8 -*-
 """ 
-Implementation of an algorithm to estimate the number 
-of distinct items in a stream as described in 
-"Probabitistic Counting Algorithms for Data Base 
-Applications" by Flajolet and Martin, 1985.
+Implementation of the Hyper Log Log algorithm to estimate the number 
+of distinct items in a stream. 
 """
 class HLL:
 
     def __init__(self, bins: int):
-        self.phi: float = 0.77351            # The magic number
-        self.nmap: int = bins                # How many bins
-        self.bitsets = [[]] * self.nmap # The pseudo - bitmap to store data
+        self.m: int = bins                          # How many bins
+        self.alpha = 0.7213 / (1 + 1.079/self.m)
+        self.bits = [0] * self.m # The pseudo - bitmap to store data
         self.resetBits()
 
     def resetBits(self):
         """Helper function to reset bits between experiments
         """
-        for i in range(0, self.nmap):
-            self.bitsets[i] = [0] * 32  # 32 bit set should be enough
+        for i in range(0, self.m):
+            self.bits[i] = 0  
 
     def query(self) -> int:
         """Returns an estimation of the number of items
         """
         result: int = 0
-        for i in range(0,self.nmap):
-            r: int = 0
-            for idx, j in enumerate(self.bitsets[i]):
-                if j == 0:
-                    r = idx
-                    break
-            result += (r)
-        return int(self.nmap / self.phi * 2 **(result / self.nmap))
+        for i in range(0,self.m):
+            result += 2**(-self.bits[i])
+        result = self.alpha * self.m**2 / result
+        if result <= 5/2 * self.m:
+            sumreg = self.bits.count(0)
+            if sumreg > 0:
+                return self.m * math.log2(self.m/sumreg)
+            else:
+                return result
+        if result <= 1/30 * 2**32:
+            return result
+        else:
+            return -2**32 * math.log2(1 - result / 2**32)
 
-    def rho(value):
+
+    def rho(self, value):
         """returns the position of the rightmost 1-bit
            rho(1)=1, rho(0001)=4 
         """
@@ -45,39 +50,39 @@ class HLL:
                 break
             bit <<= 1
             count += 1
-        return count
-
+        return count + 1
 
     def add(self, item : str):
         """Adds an item to the estimator
         """
         h: int = hash(item) % (9999999999999)
-        bin_num: int = h % self.nmap
-        hashval: int = int(h / self.nmap)
-        count = rho(hashval)
-        self.bitsets[bin_num][count] = 1
+        bin_num: int = h % self.m
+        hashval: int = int(h / self.m)
+        count = self.rho(hashval)
+        self.bits[bin_num] = max(self.bits[bin_num], count)
+
 
 if __name__ == "__main__":
-    assert(rho(int('01010101'))==1)
-    assert(rho(int('00101000'))==4)
+    hll = HLL(256)
+    assert(hll.rho(int('01010101'))==1)
+    assert(hll.rho(int('00101000'))==4)
     runs = 30
     vals = []
-    hll = HLL(16)
-    testvalues = np.logspace(0, 6, num=200, dtype=int)
+    testvalues = np.logspace(0, 5, num=200, dtype=int)
     print(testvalues)
     for run in range(1,runs+1):
-        fm85.resetBits()
+        hll.resetBits()
         avg = 0
         num = 0
         k = 0
-        for i in range(1,100000):
+        for i in range(1,10000):
             hll.add("item"+str(i)+str(run))
             if i in testvalues:
                 if run == 1:
                     vals.append(0)
                 num += 1
                 res1 = hll.query()
-                print(f"{i} {res1} - { res1 / i }")
+                print(f"{i} {int(res1)} - { res1 / i }")
                 vals[k] += res1 / i 
                 k += 1
         print("-------")
